7dc981d5a16060a23f511e9542da91303e9f72a8
Nyssa Keller <nyssa1@gmail.com>
2018-04-12 12:25:16 -0600

    Add initial migration

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,13 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/restaurant'
+    connection: 'postgres://localhost/restaurant',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds:  {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
   }
 };
