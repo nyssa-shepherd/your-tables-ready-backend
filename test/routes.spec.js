9453841e1dd5e6e0ee05ae529a1ffa87efcf57b1
Nyssa Keller <nyssa1@gmail.com>
2018-04-12 19:19:41 -0600

    Create GET /api/v1/restaurants

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -10,16 +10,37 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('API Routes', () => {
-  beforeEach((done) => {
-    database.migrate.rollback()
-      .then(() => {
-        database.migrate.latest()
-          .then(() => {
-            return database.seed.run()
-              .then(() => {
-                done();
-              });
+  // beforeEach((done) => {
+  //   database.migrate.rollback()
+  //     .then(() => {
+  //       database.migrate.latest()
+  //         .then(() => {
+  //           return database.seed.run()
+  //             .then(() => {
+  //               done();
+  //             });
+  //         });
+  //     });
+  // });
+
+  describe('RESTAURANTS endpoints', () => {
+    describe('GET /api/v1/restaurants', () => {
+      it('return all restaurants', () => {
+        return chai.request(server)
+          .get('/api/v1/restaurants')
+          .then( response => {
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('array');
+            response.body[0].should.have.property('restaurant_name');
+            response.body[0].should.have.property('username');
+            response.body[0].should.have.property('password');
+            response.body.length.should.equal(2);
+          })
+          .catch( err => {
+            throw err;
           });
       });
+    });
   });
 });
